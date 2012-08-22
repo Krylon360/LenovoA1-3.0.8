42e975dbf855982fa29fc9b70c5676e93cfe931b
eyeballer <eyeballer@teamw.in>
2012-08-22 23:54:14 +0000

    initial import from lenovo

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 PREEMPT Mon Jul 2 14:40:35 CST 2012"
+#define LINUX_COMPILE_BY "mhl"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
